7077b0d9296b6932498417b074327cc912b0c90b
Hojin Jang <onewithtruth@gmail.com>
2022-07-09 12:37:02 +0800

    Initial commit

---
+++ src/main/java/com/codestates/srdemoref/SrDemoRefApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.srdemoref;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrDemoRefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrDemoRefApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codestates/srdemoref/SrDemoRefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.srdemoref;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrDemoRefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
